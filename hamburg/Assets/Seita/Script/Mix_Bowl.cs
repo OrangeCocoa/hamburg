a5947ac2acf40b02125b8960ad0dcd7d455f330f
suneo2612 <suneo2612@users.noreply.github.com>
2019-11-09 14:30:17 +0900

    sauce

---
+++ hamburg/Assets/Seita/Script/Mix_Bowl.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ボウルまぜまぜ
+public class Mix_Bowl : MonoBehaviour
+{
+    // ==========================================================================
+    //                               メンバ変数
+    // ==========================================================================
+    // 外部入力値
+    [SerializeField]
+    private int             m_nAddScore;
+
+    // 変数
+    private BGM_Manager     m_pBGM_ManagerComponent;
+    private int             m_nScore;
+    private bool            m_bUpdate;
+    private bool            m_bGameEnd;
+    private bool            m_bGameStart;
+
+
+    // ==========================================================================
+    //                               メンバ関数
+    // ==========================================================================
+    // 更新スイッチ
+    public void SetUpdateSwitch(bool bUpdate)
+    {
+        m_bUpdate = bUpdate;
+    }
+
+    // スコア渡し
+    public int  GetMixScore()
+    {
+        return m_nScore;
+    }
+
+    // ゲーム終了渡し
+    public bool GetGameEnd()
+    {
+        return m_bGameEnd;
+    }
+
+    // ゲーム処理
+    private void Mix_Bowl_Game()
+    {
+        // もしBGMが終わっていなければ...
+        if(!m_pBGM_ManagerComponent.GetEndBgm())
+        {
+            // ゲームが開始されているのなら
+            if(m_bGameStart)
+            {
+                // BGMが終わらぬ限り
+                if(Input.GetMouseButtonDown(0) && !m_pBGM_ManagerComponent.GetEndBgm())
+                {
+                    m_nScore += m_nAddScore;
+                }
+                
+                // BGMが終わったら
+                if(m_pBGM_ManagerComponent.GetEndBgm())
+                {
+                    m_bGameEnd = true;
+                }
+            }
+
+            // もし始めの一回目でクリックがなされたなら
+            if(Input.GetMouseButtonDown(0) && !m_bGameStart)
+            {
+                m_bGameStart = true;
+                m_pBGM_ManagerComponent.BGM_Switch(true);
+            }
+        }
+        else
+        {
+            // 終わりスイッチ
+            m_bGameEnd = true;
+        }
+    }
+
+
+    // ==========================================================================
+    //                                基本関数
+    // ==========================================================================
+    // 初期化
+    // Start is called before the first frame update
+    void Start()
+    {
+        // コンポーネント取得
+        m_pBGM_ManagerComponent = GetComponent<BGM_Manager>();
+
+        // 初期化
+        m_bUpdate = false;
+        m_bGameStart = false;
+        m_bGameEnd = false;
+    }
+
+    // 更新
+    // Update is called once per frame
+    void Update()
+    {
+        // 更新
+        if(m_bUpdate)
+        {
+            // ゲーム処理
+            Mix_Bowl_Game();
+        }
+    }
+}
---
+++ hamburg/Assets/Seita/Script/Source_Color.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ソースの色変え
+public class Source_Color : MonoBehaviour
+{
+    // ==========================================================================
+    //                               メンバ変数
+    // ==========================================================================
+    // 変数
+    private SpriteRenderer      m_pSpriteRendererComponent;     // スプライトレンダラーコンポーネント
+
+
+    // ==========================================================================
+    //                               メンバ関数
+    // ==========================================================================
+    // 色変え
+    public  void SetSourceColor(Color   pSourceColor)
+    {
+        // コンポーネントの取得ができているなら
+        if (m_pSpriteRendererComponent != null)
+        {
+            m_pSpriteRendererComponent.color = pSourceColor;
+        }
+    }
+
+
+    // ==========================================================================
+    //                                基本関数
+    // ==========================================================================
+    // 初期化
+    // Start is called before the first frame update
+    void Start()
+    {
+        // コンポーネント取得
+        m_pSpriteRendererComponent = GetComponent<SpriteRenderer>();
+    }
+
+    // 更新
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
